7fa4aecbb43285da4da4a0dd550272cfd981c39e
yacob hae <yacobhae@gmail.com>
2023-10-25 15:02:06 +0700

    add new output librabry

--- src/programyakob/jenkins/Output.groovy
+++ src/programyakob/jenkins/Output.groovy
@@ -2,7 +2,7 @@ package programyakob.jenkins;
 
 class Output{
     static def hello(String name){
-        echo("Hello ${name}")
-        // println("Hello ${name}");
+        // echo("Hello ${name}")
+        println("Hello ${name}")
     }
 }
