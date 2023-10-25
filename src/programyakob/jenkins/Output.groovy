d3ed870fb451c7e9b45da7473b2993782274e242
yacob hae <yacobhae@gmail.com>
2023-10-25 15:10:36 +0700

    add parameters steps

--- src/programyakob/jenkins/Output.groovy
+++ src/programyakob/jenkins/Output.groovy
@@ -1,8 +1,9 @@
 package programyakob.jenkins;
 
 class Output{
-    static def hello(String name){
+    static def hello(steps, String name){
         // echo("Hello ${name}")
-        println("Hello ${name}")
+        // println("Hello ${name}")
+        steps.echo("Hello ${name}")
     }
 }
