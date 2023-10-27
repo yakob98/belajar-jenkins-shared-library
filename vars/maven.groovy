3d5465c7a9af73ac8b77b786250739c50ee469ea
yacob hae <yacobhae@gmail.com>
2023-10-27 16:07:27 +0700

    Map stages

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -2,6 +2,12 @@
 //   sh("./mvnw ${testing}")
 // }
 
+// def call(String commands){
+//     for(command in commands){
+//      sh("./mvnw ${command}")        
+//     }
+// }
+
 def call(List commands){
     for(command in commands){
      sh("./mvnw ${command}")        
